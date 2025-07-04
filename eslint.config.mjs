2d2cb6d62da687f658931c7e69b7eed8e75ef219
Krishan Kumar <KrishanKumar940097@gmail.com>
2025-07-04 09:40:00 +0530

    chore(config): initialize prettier and eslint rulesets

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  {
+    rules: {
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_" },
+      ],
+      "@typescript-eslint/no-explicit-any": "warn",
+      "react/react-in-jsx-scope": "off",
+      "@tailwindcss/no-whitespace-before-unit": "off",
+      "@tailwindcss/invalid-class-name": "off",
+      "@tailwindcss/redundant-cursor-pointer": "off",
+    },
+  },
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+    "node_modules/**",
+  ]),
+]);
+
+export default eslintConfig;
